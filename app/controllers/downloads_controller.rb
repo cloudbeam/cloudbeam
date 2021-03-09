7d352b3227b4a1bf951f818bc53282e19d4ab799
Elizabeth Tackett <emctackett@gmail.com>
2021-03-09 11:33:45 -0600

    WIP

--- app/controllers/downloads_controller.rb
+++ app/controllers/downloads_controller.rb
@@ -1,4 +1,18 @@
+require 'date'
+
 class DownloadsController < ApplicationController
   def show
+    file_name = "graham-cookies.zip"
+    resource = "https://d1u2b9f4x0ygob.cloudfront.net/#{file_name}?response-content-disposition=attachment;"
+
+    signer = Aws::CloudFront::UrlSigner.new({
+        key_pair_id: Rails.application.credentials.cloudfront[:public_key_id],
+        response_content_disposition: "attachment",
+        private_key: Rails.application.credentials.cloudfront[:private_key]
+      })
+
+    @signed_url = signer.signed_url(resource, expires: Date.today + 1)
+    puts "*****************\nSigned URL: #{@signed_url}\n********************"
+    redirect_to @signed_url
   end
 end
