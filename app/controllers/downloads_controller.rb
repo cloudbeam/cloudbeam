73bbb0ff734438d995d94760023744000b499121
Jimmy Zheng <jimmy_842@berkeley.edu>
2021-03-11 10:31:10 -0800

    commit for merge

--- app/controllers/downloads_controller.rb
+++ app/controllers/downloads_controller.rb
@@ -1,21 +1,21 @@
 require 'date'
 
 class DownloadsController < ApplicationController
-  # def show
-  #   # dummy data for now. needs to be integrated with the database.
-  #   file_name = "graham-cookies.zip"
-  #   resource = "https://dvt9gv73mq47e.cloudfront.net/#{file_name}?response-content-disposition=attachment;"
+  def show
+    # dummy data for now. needs to be integrated with the database.
+    file_name = "graham-cookies.zip"
+    resource = "https://dvt9gv73mq47e.cloudfront.net/#{file_name}?response-content-disposition=attachment;"
 
-  #   signer = Aws::CloudFront::UrlSigner.new({
-  #       key_pair_id: Rails.application.credentials.cloudfront[:public_key_id],
-  #       response_content_disposition: "attachment",
-  #       private_key: Rails.application.credentials.cloudfront[:private_key]
-  #     })
+    signer = Aws::CloudFront::UrlSigner.new({
+        key_pair_id: Rails.application.credentials.cloudfront[:public_key_id],
+        response_content_disposition: "attachment",
+        private_key: Rails.application.credentials.cloudfront[:private_key]
+      })
 
-  #   # update for shorter expiration time
-  #   @signed_url = signer.signed_url(resource, expires: Date.today + 1)
-  #   redirect_to @signed_url
-  # end
+    # update for shorter expiration time
+    @signed_url = signer.signed_url(resource, expires: Date.today + 1)
+    redirect_to @signed_url
+  end
 
   def index
     # get any id param in the url
@@ -34,8 +34,23 @@ class DownloadsController < ApplicationController
         document = Document.find(document_recipient.document_id)
         if document_recipient.downloaded_at == nil then
           @notice = "Starting"
+          document_recipient.downloaded_at = DateTime.now
+          name = "homepage.jpg"
+          resource = "https://dvt9gv73mq47e.cloudfront.net/#{name}?response-content-disposition=attachment;"
+
+          signer = Aws::CloudFront::UrlSigner.new({
+            key_pair_id: Rails.application.credentials.cloudfront[:public_key_id],
+            response_content_disposition: "attachment",
+            private_key: Rails.application.credentials.cloudfront[:private_key]
+          })
+
+          # update for shorter expiration time
+          @signed_url = signer.signed_url(resource, expires: Date.today + 1)
+          redirect_to @signed_url
 
           user = User.find(document.user_id)
+          
+          return
 
         elsif document.expired_at then
           @notice = "File expired"
