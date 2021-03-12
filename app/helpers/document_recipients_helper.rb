c6224d6aea6b7787aa139d0cec62d585a1826fd6
Kyle <kaledoux@gmail.com>
2021-03-12 10:00:11 -0500

    File Management - Tracking Tab - Send Again Extracted to Helper

--- app/helpers/document_recipients_helper.rb
+++ app/helpers/document_recipients_helper.rb
@@ -3,13 +3,16 @@ module DocumentRecipientsHelper
     recipient.shared_at.localtime.to_formatted_s(:long)
   end
 
+  def did_recipient_use_download_code(recipient)
+    recipient.downloaded_at.nil? ? 'No' : 'Yes'
+  end
+
   def create_download_again_link(recipient)
-    "<%= #{recipient.downloaded_at} == nil ? 'No' : 'Yes' %>
-    <br>
-      <button class='text-blue-400 background-transparent font-normal text-sm outline-none focus:outline-none' 
-        type='button' 
-        style='transition: all .15s ease'>
-          send again
-      </button>"
+    return unless recipient.downloaded_at.nil?
+
+    button_tag 'Send Again',
+               type: 'button',
+               class: 'text-blue-400 background-transparent font-normal text-sm outline-none focus:outline-none',
+               confirm: "This will send another email to #{recipient.email}. Are you sure?"
   end
 end
