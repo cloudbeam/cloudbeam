fa458e718b0c85c4060a30ee3f32c6c0e2945016
Elizabeth Tackett <emctackett@gmail.com>
2021-03-14 08:57:14 -0500

    remove integration test from unit testing

--- app/models/document.rb
+++ app/models/document.rb
@@ -3,9 +3,7 @@ class Document < ApplicationRecord
   belongs_to :user
   has_many :document_recipients, dependent: :destroy
 
-  # stow away
   S3_BUCKET_BASE_URL = Rails.application.credentials[:bucket_url]
-  #S3_BUCKET_BASE_URL = "https://cloudbeam.s3.us-east-2.amazonaws.com/"
 
   # create url to access file in bucket
   def calculate_s3_url(s3_key, base_url)
--- test/controllers/documents_controller_test.rb
+++ test/controllers/documents_controller_test.rb
@@ -24,16 +24,6 @@ class DocumentsControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
-  test "should create document" do
-    assert_difference('Document.count') do
-      test_doc = fixture_file_upload('files/upload-tester.json', 'application/json')
-
-      post documents_url, params: { document: { name: 'test-doc-1', upload: test_doc, user_id: 1 } }
-    end
-
-    assert_redirected_to document_url(Document.last)
-  end
-
   test "should show document" do
     get document_url(@document)
     assert_response :success
