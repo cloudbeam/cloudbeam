f04533152199d8be304f3160494b089a7c0c08f3
Jimmy Zheng <jimmy_842@berkeley.edu>
2021-03-16 11:23:23 -0700

    restore users_controller version and remove scripts from show.html.erb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -25,6 +25,8 @@ class UsersController < ApplicationController
 
     respond_to do |format|
       if @user.save
+        session[:user_id] = @user.id
+        cookies.signed[:user_id] = @user.id
         format.html { redirect_to login_url, success: "User was successfully created." }
         format.json { render :show, status: :created, location: @user }
       else
