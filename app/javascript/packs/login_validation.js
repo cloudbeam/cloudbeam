06f8d51ca3a01abca7c29fab360a87fb60c17311
Jimmy Zheng <jimmy_842@berkeley.edu>
2021-03-17 11:42:41 -0700

    refactored login and signup validation

--- app/javascript/packs/login_validation.js
+++ app/javascript/packs/login_validation.js
@@ -1,56 +1,62 @@
-self.email = document.querySelector('#user_email');
-self.password = document.querySelector('#user_password');
-self.loginForm = document.querySelector('#login-form');
-self.loginBtn = document.querySelector('#login-btn');
-self.inputs = {
-  email: self.email,
-  password: self.password
-};
+document.addEventListener('turbolinks:load', e => {
+  const email = document.querySelector('#user_email');
+  const password = document.querySelector('#user_password');
+  const loginBtn = document.querySelector('#login-btn');
 
-self.validationTable = {
-  email: function(input) {
-    if (input.match(/^.+@.+$/)) {
-      return true;
-    } else if (input == "") {
-      return "Please enter an email";
-    } else {
-      return "Please enter a valid email";
-    }
-  },
+  if (!loginBtn) {
+    return;
+  }
+  const inputs = {
+    email: email,
+    password: password
+  };
 
-  password: function(input) {
-    if (input == "") {
-      return "Please enter a password";
-    } else {
-      return true;
+  const validationTable = {
+    email: function(input) {
+      if (input.match(/^.+@.+$/)) {
+        return true;
+      } else if (input == "") {
+        return "Please enter an email";
+      } else {
+        return "Please enter a valid email";
+      }
+    },
+
+    password: function(input) {
+      if (input == "") {
+        return "Please enter a password";
+      } else {
+        return true;
+      }
     }
   }
-}
 
-function validateInput(inputName) {
-  let inputValue = self.inputs[inputName].value;
-  let result = self.validationTable[inputName](inputValue);
-  if (result !== true) {
-    self.inputs[inputName].nextElementSibling.textContent = result;
-    return false;
+  function validateInput(inputName) {
+    let inputValue = inputs[inputName].value;
+    let result = validationTable[inputName](inputValue);
+    if (result !== true) {
+      inputs[inputName].nextElementSibling.textContent = result;
+      return false;
+    }
+    inputs[inputName].nextElementSibling.textContent = "";
+    return true;
   }
-  self.inputs[inputName].nextElementSibling.textContent = "";
-  return true;
-}
 
-for (let prop in self.inputs) {
-  let element = self.inputs[prop];
-  element.addEventListener('blur', e => {
-    e.preventDefault();
-    validateInput(prop);
-  });
-}
-
-self.loginBtn.addEventListener('click', e => {
-  for (let prop in self.inputs) {
-    let validation = validateInput(prop);
-    if (!validation) {
+  for (let prop in inputs) {
+    let element = inputs[prop];
+    element.addEventListener('blur', e => {
       e.preventDefault();
-    }
+      validateInput(prop);
+    });
   }
+
+  loginBtn.addEventListener('click', e => {
+    console.log('clicked');
+    for (let prop in self.inputs) {
+      let validation = validateInput(prop);
+      if (!validation) {
+        e.preventDefault();
+      }
+    }
+  });
 });
