77f1700ac42ae154fc2bd69fc283b37f12ed3738
Elizabeth Tackett <emctackett@gmail.com>
2021-03-19 10:46:12 -0500

    add session timeouts

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,8 @@
 class SessionsController < ApplicationController
   skip_before_action :verify_authenticity_token
+  skip_before_action :session_expires
+  skip_before_action :update_session_time
+
   def new
     if session[:user_id] then
       redirect_to documents_dashboard_url
@@ -11,7 +14,9 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:user][:email])
     if user.try(:authenticate, params[:user][:password]) then
       session[:user_id] = user.id
+      session[:expires_at] = 60.minutes.from_now
       cookies.signed[:user_id] = user.id
+
       redirect_to documents_dashboard_url
     else
       redirect_to login_url, alert: "Invalid user/password combination"
@@ -23,7 +28,7 @@ class SessionsController < ApplicationController
       session[:user_id] = nil
       cookies.delete(:user_id)
       redirect_to login_url, notice: "Logged out"
-    else 
+    else
       redirect_to download_url
     end
   end
