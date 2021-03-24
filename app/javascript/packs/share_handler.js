85b3929849da3181a3b09c45ae3e5b019f7703fb
Elizabeth Tackett <emctackett@gmail.com>
2021-03-24 09:40:38 -0500

    debug handlers

--- app/javascript/packs/share_handler.js
+++ app/javascript/packs/share_handler.js
@@ -26,10 +26,9 @@ function validateInput(inputName) {
 }
 
 for (let prop in inputs) {
-	console.log(prop);
 	let element = inputs[prop];
 	element.addEventListener('blur', (e) => {
-		e.preventDefault(); 
+		e.preventDefault();
 		validateInput(prop);
 	});
 }
