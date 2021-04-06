c27fcf9ca10be5023079886cfc60336852ff3332
Elizabeth Tackett <emctackett@gmail.com>
2021-04-06 09:18:01 -0500

    update to designer font and color

--- app/javascript/stylesheets/tailwind.config.js
+++ app/javascript/stylesheets/tailwind.config.js
@@ -12,17 +12,17 @@ module.exports = {
 	theme    : {
 		extend : {
 			fontFamily      : {
-				sans : [ 'Inter var', ...defaultTheme.fontFamily.sans ]
+				'body' : [ '"Open Sans Light"', ...defaultTheme.fontFamily.sans ]
 			},
 			colors          : {
-				pale_yellow : '#FBE8A6',
+				pale_yellow : '#FFC544',
 				orange      : '#F27D42',
 				navy        : '#182a44',
 				light_navy  : '#f0f7ff',
-				turqoise    : '#55BDCA',
+				turqoise    : '#0094FE',
 				light_blue  : '#96FFFF',
 				powder_blue : '#C8EFF9',
-				white       : '#ffffff'
+				white       : '#FFFFFF'
 			},
 			// backgroundImage : (theme) => ({
 			// 	'cloud-gen' : "url('./../../assets/images/cloud-background.jpg')"
